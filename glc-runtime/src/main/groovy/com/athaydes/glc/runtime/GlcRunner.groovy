919f3baf8a8202250eb1f1fa2edce952361858f2
renato <renato@twobo.com>
2016-10-09 12:55:09 +0200

    Implemented basic GLC runner.

--- glc-runtime/src/main/groovy/com/athaydes/glc/runtime/GlcRunner.groovy
+++ glc-runtime/src/main/groovy/com/athaydes/glc/runtime/GlcRunner.groovy
@@ -1,14 +1,87 @@
 package com.athaydes.glc.runtime
 
+import com.athaydes.glc.procedure.GlcProcedure
+import com.athaydes.glc.procedure.GlcProcedureParameter
 import com.athaydes.glc.procedure.GlcProcedures
+import groovy.transform.CompileDynamic
 import groovy.transform.CompileStatic
+import groovy.transform.PackageScope
+import groovy.util.logging.Slf4j
 
+import java.util.concurrent.ConcurrentHashMap
+
+@Slf4j
 @CompileStatic
 class GlcRunner {
 
+    private final Map<GlcProcedureParameter, Object> valueByParameter = new ConcurrentHashMap<>()
+
     void run( GlcProcedures glcProcedures ) {
+        initialRunOf glcProcedures
+        cycle glcProcedures, valueByParameter
+    }
+
+    private void initialRunOf( GlcProcedures glcProcedures ) {
         final immediateRun = glcProcedures.emptyInputProcedures
-        immediateRun*.runnable*.call()
+
+        immediateRun.parallelStream().forEach { GlcProcedure procedure ->
+            def value = procedure.runnable.call()
+            valueByParameter[ procedure.output ] = value
+        }
+
+        log.debug( "Values initialized in the initial run: {}", valueByParameter )
+    }
+
+    private void cycle( GlcProcedures glcProcedures,
+                        Map<GlcProcedureParameter, Object> changedValues ) {
+        if ( changedValues.isEmpty() ) {
+            log.info( "No more input changes possible, terminating GLC Program" )
+            return
+        }
+
+        log.debug( "Changed values: {}", changedValues )
+
+        Map<String, GlcProcedure> procedureByName = [ : ]
+        Map<String, List<Object>> argumentsByProcName = [ : ]
+
+        changedValues.each { GlcProcedureParameter parameter, Object currentValue ->
+            final procedureToRun = glcProcedures.getProcedureReading( parameter )
+            if ( procedureToRun.isPresent() ) {
+                final proc = procedureToRun.get()
+                def alreadyVisited = procedureByName.put( proc.closureName, proc )
+                if ( !alreadyVisited ) {
+                    def args = proc.inputs.collect { input ->
+                        ( input == parameter ) ?
+                                currentValue :
+                                changedValues.computeIfAbsent( ( GlcProcedureParameter ) input, { p -> valueByParameter[ p ] } )
+                    }
+                    argumentsByProcName.put( proc.closureName, args )
+                }
+            }
+        }
+
+        Map<GlcProcedureParameter, Object> newChangedValues = new ConcurrentHashMap<>()
+
+        argumentsByProcName.entrySet().parallelStream().forEach { Map.Entry<String, List<Object>> entry ->
+            def procedure = procedureByName[ entry.key ]
+            def args = entry.value
+            def newValue = apply( procedure, args )
+            newChangedValues[ procedure.output ] = newValue
+            valueByParameter[ procedure.output ] = newValue
+        }
+
+        cycle( glcProcedures, newChangedValues )
+    }
+
+    @CompileDynamic
+    // allows calling the closure without type-checking
+    private static apply( GlcProcedure procedure, List<Object> args ) {
+        procedure.runnable.call( *args )
+    }
+
+    @PackageScope
+    def valueOf( GlcProcedureParameter parameter ) {
+        valueByParameter[ parameter ]
     }
 
 }
--- glc-runtime/src/test/groovy/com/athaydes/glc/runtime/GlcRunnerSpec.groovy
+++ glc-runtime/src/test/groovy/com/athaydes/glc/runtime/GlcRunnerSpec.groovy
@@ -19,14 +19,18 @@ class GlcRunnerSpec extends Specification {
         def counter2 = 0
         def counter3 = 0
 
-        def runnable1 = { counter1++ }
-        def runnable2 = { counter2++ }
-        def runnable3 = { counter3++ }
+        def runnable1 = { counter1++; 'AA' }
+        def runnable2 = { counter2++; 2 }
+        def runnable3 = { counter3++; 3 }
+
+        def parameterA = GlcParameter( String, 'a' )
+        def parameterI = GlcParameter( Integer, 'i' )
+        def parameterJ = GlcParameter( Integer, 'j' )
 
         def procedures = [
-                createGlcProcedure( 'procedure1', [ ], null, runnable1 ),
-                createGlcProcedure( 'procedure2', [ ], null, runnable2 ),
-                createGlcProcedure( 'procedure3', [ ], null, runnable3 )
+                GlcProcedure( 'procedure1', [ ], parameterA, runnable1 ),
+                GlcProcedure( 'procedure2', [ ], parameterI, runnable2 ),
+                GlcProcedure( 'procedure3', [ ], parameterJ, runnable3 )
         ]
 
         when: 'The GLC Runner runs those procedures'
@@ -36,17 +40,47 @@ class GlcRunnerSpec extends Specification {
         counter1 == 1
         counter2 == 1
         counter3 == 1
+
+        and: 'The value of each parameter is correctly assigned'
+        glcRunner.valueOf( parameterA ) == 'AA'
+        glcRunner.valueOf( parameterI ) == 2
+        glcRunner.valueOf( parameterJ ) == 3
     }
 
-    GlcProcedure createGlcProcedure( String name,
-                                     List<GlcProcedureParameter> inputs = [ ],
-                                     GlcProcedureParameter output = null,
-                                     Closure runnable = { -> } ) {
-        if ( output == null ) {
-            output = new GlcProcedureParameter( new GenericType( String, GenericType.EMPTY ), 'out' )
-        }
+    def "GLC should run procedures until no input changes are possible"() {
+        given: 'A few GLC procedures that cause a few cycles to run'
+        def runnable1 = { 123 }
+        def runnable2 = { Integer i -> i + 321 }
+        def runnable3 = { Integer j -> j.toString() }
 
-        return new GlcProcedure( new CompiledGlcProcedure( name, inputs, output ), runnable )
+        def parameterI = GlcParameter( Integer, 'i' )
+        def parameterJ = GlcParameter( Integer, 'j' )
+        def parameterS = GlcParameter( String, 's' )
+
+        def procedures = [
+                GlcProcedure( 'procedure1', [ ], parameterI, runnable1 ),
+                GlcProcedure( 'procedure2', [ parameterI ], parameterJ, runnable2 ),
+                GlcProcedure( 'procedure3', [ parameterJ ], parameterS, runnable3 )
+        ]
+
+        when: 'The GLC Runner runs those procedures'
+        glcRunner.run( new GlcProcedures( procedures ) )
+
+        then: 'The value of each parameter is correctly assigned'
+        glcRunner.valueOf( parameterI ) == 123
+        glcRunner.valueOf( parameterJ ) == 444
+        glcRunner.valueOf( parameterS ) == '444'
+    }
+
+    GlcProcedure GlcProcedure( String name,
+                               List<GlcProcedureParameter> inputs,
+                               GlcProcedureParameter output,
+                               Closure runnable ) {
+        new GlcProcedure( new CompiledGlcProcedure( name, inputs, output ), runnable )
+    }
+
+    GlcProcedureParameter GlcParameter( Class type, String name ) {
+        new GlcProcedureParameter( new GenericType( type, GenericType.EMPTY ), name )
     }
 
 }
